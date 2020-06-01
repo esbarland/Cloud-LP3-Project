b1e735c1582359b2ce6fb3bc202b7d7f11c4c5b5
thbailly <thibaut.bailly18@gmail.com>
2020-06-01 21:08:42 +0200

    Add updateAmount method

--- Cloude-Service/src/main/java/services/AccountService.java
+++ Cloude-Service/src/main/java/services/AccountService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -29,10 +30,10 @@ public class AccountService {
 	}
 
 	@POST
-	@Path("{lastname}/{firstname}/{amount}")
+	@Path("{lastname}/{firstname}/{amount}/{risk}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String addAccount(@PathParam("lastname") String lastname, @PathParam("firstname") String firstname, @PathParam("amount") int amount) {
-		Account a = new Account(firstname, lastname, amount, "low");
+	public String addAccount(@PathParam("lastname") String lastname, @PathParam("firstname") String firstname, @PathParam("amount") int amount, @PathParam("risk") String risk) {
+		Account a = new Account(firstname, lastname, amount, risk);
 		ObjectifyService.ofy().save().entity(a).now();
 		
 		Gson gson = new Gson();
@@ -48,5 +49,18 @@ public class AccountService {
 		
 		return "deleted";
 	}
+	
+	@PUT
+	@Path("{id}/{amount}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String updateAmount(@PathParam("id") Long id, @PathParam("amount")int amount) {
+		Account a = ObjectifyService.ofy().load().type(Account.class).id(id).now();
+		a.setAmount(a.getAmount() + amount);
+		Gson gson = new Gson();
+		String json = gson.toJson(a);
+		return json;
+		
+	}
+
 	
 }
